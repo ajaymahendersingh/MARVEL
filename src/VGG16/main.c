a5793daea59775067b4668adf92b7349762a68e0
ajaymahendersingh <123837228+ajaymahendersingh@users.noreply.github.com>
2025-02-27 14:58:02 +0000

    Create main.c

---
+++ src/VGG16/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "model/samples/car.c"
+#include "model/samples/catan.c"
+#include "tvmgen_default.h"
+
+uint8_t output1[2];
+uint8_t output2[2];
+
+int main(void){
+
+    struct tvmgen_default_inputs inputs = {
+            .serving_default_input_2_0 = (void*)&CAR_IMAGE,
+    };
+    struct tvmgen_default_outputs outputs1 = {
+            .StatefulPartitionedCall_0 = (void*)&output1,
+    };
+
+    struct tvmgen_default_outputs outputs2 = {
+            .StatefulPartitionedCall_0 = (void*)&output2,
+    };
+
+    printf("Evaluating VGG16 model using microTVM:\n");
+
+    inputs.serving_default_input_2_0 = (void*)&CAR_IMAGE;
+    tvmgen_default_run(&inputs, &outputs1);
+    printf("Car Result: random = %d, car = %d\n", output1[0], output1[1]);
+
+    inputs.serving_default_input_2_0 = (void*)&CATAN_IMAGE;
+    tvmgen_default_run(&inputs, &outputs2);
+    printf("Random Result: random = %d, car = %d\n", output2[0], output2[1]);
+  
+    return 0;
+}
