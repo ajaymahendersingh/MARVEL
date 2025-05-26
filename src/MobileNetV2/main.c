e64b09166371f884a1264b9e06df90ea1d5c31c7
ajaymahendersingh <123837228+ajaymahendersingh@users.noreply.github.com>
2025-05-26 16:44:03 +0100

    Update main.c

--- src/MobileNetV2/main.c
+++ src/MobileNetV2/main.c
@@ -28,26 +28,6 @@ int main(void){
     inputs.serving_default_input_2_0 = (void*)&CATAN_IMAGE;
     tvmgen_default_run(&inputs, &outputs2);
     printf("Random Result: random = %d, car = %d\n", output2[0], output2[1]);
-
-    if(output1[0] < 50 && output1[1] > 200) {
-        asm volatile ("lui x25, 0xBA55");
-        asm volatile ( "li x26, 0xBA");
-        asm volatile ( "or x25, x25, x26");
-    } else {
-        asm volatile ("lui x25, 0xDEAD");
-        asm volatile ( "li x26, 0xBE");
-        asm volatile ( "or x25, x25, x26");
-    }
-
-    if(output2[1] < 50 && output2[0] > 200) {
-        asm volatile ("lui x26, 0xBA55");
-        asm volatile ( "li x27, 0xBA");
-        asm volatile ( "or x26, x26, x27");
-    } else {
-        asm volatile ("lui x26, 0xDEAD");
-        asm volatile ( "li x27, 0xBE");
-        asm volatile ( "or x26, x26, x27");
-    }
     
     return 0;
 }
